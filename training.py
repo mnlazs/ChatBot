6c9da0beeee2bdf6817bf68a5921cf1143394757
mnlazs <mnl.azs@gmail.com>
2024-03-17 11:57:06 -0500

    updated

--- training.py
+++ training.py
@@ -5,3 +5,10 @@ import numpy as np
 
 import nltk
 from nltk.stem import WordNetLemmatizer
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD 
+
+
+
